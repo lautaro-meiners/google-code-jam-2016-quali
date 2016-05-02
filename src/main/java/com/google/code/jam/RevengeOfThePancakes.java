363106a4e4b9b695b8b934ada330e7079a8464d1
Lautaro Meiners <lautaromeiners@gmail.com>
2016-05-02 17:03:21 -0300

    Agrega la clase RevengeOfThePancakes y su clase main

---
+++ src/main/java/com/google/code/jam/RevengeOfThePancakes.java
@@ -0,0 +1,19 @@
+package com.google.code.jam;
+
+import java.nio.file.Paths;
+
+/**
+ * Created by lautaro.meiners on 02/05/2016.
+ * Link al ejercicio en la pagina de google: https://code.google.com/codejam/contest/6254486/dashboard#s=p1
+ */
+public class RevengeOfThePancakes extends CodeJam {
+
+    String solve(String line) {
+        return null;
+    }
+
+    public static void main (String[] args) throws Exception {
+        RevengeOfThePancakes revengeOfThePancakes = new RevengeOfThePancakes();
+        revengeOfThePancakes.run(Paths.get(args[0]));
+    }
+}
